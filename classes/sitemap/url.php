bf62dc0921c2114cb0ec5d8ef7a636e224a6d072
Mathew Davies <thepixeldeveloper@googlemail.com>
2010-06-17 23:26:54 +0100

    Cast $change_frequency to a string so a value of 0 or TRUE wont pass.

--- classes/sitemap/url.php
+++ classes/sitemap/url.php
@@ -77,6 +77,8 @@ class Sitemap_URL extends Sitemap_Data
 	 */
 	public function set_change_frequency($change_frequency)
 	{
+		$change_frequency = (string) $change_frequency;
+		
 		$frequencies = array('always', 'hourly', 'daily', 'weekly', 'monthly', 'yearly', 'never');
 
 		if ( ! in_array($change_frequency, $frequencies))
