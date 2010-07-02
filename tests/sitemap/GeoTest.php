2afcfdac99a3580662e6e290a0a0b3c0a078187a
Mathew Davies <thepixeldeveloper@googlemail.com>
2010-07-02 20:55:09 +0100

    Add root tests for the Geo sitemap.

--- tests/sitemap/GeoTest.php
+++ tests/sitemap/GeoTest.php
@@ -75,4 +75,28 @@ class Sitemap_GeoTest extends PHPUnit_Framework_TestCase
 		$this->assertSame(TRUE, property_exists($xml, 'geo:geo'));
 		$this->assertSame(TRUE, property_exists($xml->{"geo:geo"}, 'geo:format'));
 	}
+
+	/**
+	 * @test
+	 * @group sitemap
+	 */
+	public function test_root()
+	{
+		// Base Sitemap
+		$sitemap = new Sitemap;
+
+		// Create basic Mobile Sitemap
+		$instance = new Sitemap_URL(new Sitemap_Geo);
+		$instance->set_loc('http://google.com');
+		$sitemap->add($instance);
+
+		// Load the end XML
+		$xml = new SimpleXMLElement($sitemap->render());
+
+		// Namespaces.
+		$namespaces = $xml->getDocNamespaces();
+
+		$this->assertSame(TRUE, isset($namespaces['geo']));
+		$this->assertSame('http://www.google.com/geo/schemas/sitemap/1.0', $namespaces['geo']);
+	}
 }
