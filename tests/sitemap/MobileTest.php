a72ff6341c751dbfdc901f72588c243446d58705
Mathew Davies <thepixeldeveloper@googlemail.com>
2010-07-02 20:53:36 +0100

    Adding root test for the mobile sitemap.

--- tests/sitemap/MobileTest.php
+++ tests/sitemap/MobileTest.php
@@ -23,4 +23,28 @@ class Sitemap_MobileTest extends PHPUnit_Framework_TestCase
 		$xml = simplexml_import_dom($result);
 		$this->assertSame(TRUE, property_exists($xml, 'mobile:mobile'));
 	}
+
+	/**
+	 * @test
+	 * @group sitemap
+	 */
+	public function test_root()
+	{
+		// Base Sitemap
+		$sitemap = new Sitemap;
+
+		// Create basic Mobile Sitemap
+		$instance = new Sitemap_URL(new Sitemap_Mobile);
+		$instance->set_loc('http://google.com');
+		$sitemap->add($instance);
+
+		// Load the end XML
+		$xml = new SimpleXMLElement($sitemap->render());
+
+		// Namespaces.
+		$namespaces = $xml->getDocNamespaces();
+
+		$this->assertSame(TRUE, isset($namespaces['mobile']));
+		$this->assertSame('http://www.google.com/schemas/sitemap-mobile/1.0', $namespaces['mobile']);
+	}
 }
