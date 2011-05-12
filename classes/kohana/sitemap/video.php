498ba7dc41706f1ea695d875961b61c8f28e54cf
Mathew Davies <thepixeldeveloper@googlemail.com>
2011-05-12 16:18:47 +0100

    Fleshed out and nearly completed the video sitemap. Todos specify what needs completing.

--- classes/kohana/sitemap/video.php
+++ classes/kohana/sitemap/video.php
@@ -2,24 +2,23 @@
 
 abstract class Kohana_Sitemap_Video implements Kohana_Sitemap_Interface
 {
-
-	// Attributes
 	private $_attributes = array
 	(
-		'player_loc'			 => NULL,
-		'content_loc'			 => NULL,
+		
 		'thumbnail_loc'		 => NULL,
 		'title'						 => NULL,
 		'description'			 => NULL,
+		'content_loc'			 => NULL,
+		'expiration_date'  => NULL,
 		'rating'					 => NULL,
 		'view_count'			 => NULL,
 		'publication_date' => NULL,
-		'tag'							 => NULL,
 		'category'				 => NULL,
 		'family_friendly'  => NULL,
-		'duration'				 => NULL,
-		'expiration_date'	 => NULL
+		'requires_subscription' => NULL
 	);
+	
+	protected $_player_loc = array();
 
 	/**
 	 * @param string $thumbnail_loc A URL pointing to the URL for the video
@@ -36,16 +35,87 @@ abstract class Kohana_Sitemap_Video implements Kohana_Sitemap_Interface
 	 */
 	public function set_title($title)
 	{
+		if (strlen($title) > 100)
+		{
+			throw new InvalidArgumentException('Your title can be no longer than 100 characters');
+		}
+		
 		$this->_attributes['title'] = $title;
 	}
-
+	
 	/**
 	 * @param string $description The description of the video. Descriptions
 	 * longer than 2048 characters will be truncated.
 	 */
 	public function set_description($description)
 	{
+		if (strlen($description) > 2048)
+		{
+			throw new InvalidArgumentException('Your description can be no longer than 2048 characters');
+		}
+		
 		$this->_attributes['description'] = $description;
+	}
+	
+	/**
+	 * @param string $loc 
+	 * @see http://www.google.com/support/webmasters/bin/answer.py?answer=80472
+	 */
+	public function set_content_loc($loc)
+	{
+		if ( ! filter_var($loc, FILTER_VALIDATE_URL))
+		{
+			throw new InvalidArgumentException($loc. ' is not a valid URL');
+		}
+				
+		$this->_attributes['content_loc'] = $loc;
+	}
+	
+	/**
+	 * @param string $loc
+	 * @param boolean $allow_embed
+	 * @param string $autostart 
+	 * @see http://www.google.com/support/webmasters/bin/answer.py?answer=80472
+	 */
+	public function set_player_loc($loc, $allow_embed, $autostart)
+	{
+		if ( ! filter_var($loc, FILTER_VALIDATE_URL))
+		{
+			throw new InvalidArgumentException($loc. ' is not a valid URL');
+		}
+		
+		$allow_embed = $allow_embed ? 'yes' : 'no';
+		
+		$this->_player_loc['loc']         = $loc;
+		$this->_player_loc['allow_embed'] = $allow_embed;
+		$this->_player_loc['autostart']   = $autostart;
+	}
+	
+	/**
+	 * @param integer $duration The duration of the video in seconds. Value must
+	 * be between 0 and 28800 (8 hours). Non-digit characters are disallowed.
+	 */
+	public function set_duration($duration)
+	{
+		$eight_hours = 28800;
+		
+		$duration = (int) $duration;
+		
+		if ($duration > $eight_hours OR $duration < 0)
+		{
+			throw new InvalidArgumentException('Duration must be between 0 and 8 hours');
+		}
+		
+		$this->_attributes['duration'] = $duration;
+	}
+	
+	/**
+	 * @param integer $expiration_date The date after which the video will no
+	 * longer be available, in unixtimestamp format.
+	 */
+	public function set_expiration_date($expiration_date)
+	{
+		$this->_attributes['expiration_date'] = $this->date_format($expiration_date);
 	}
 
 	/**
@@ -54,7 +124,22 @@ abstract class Kohana_Sitemap_Video implements Kohana_Sitemap_Interface
 	 */
 	public function set_rating($rating)
 	{
+		if ($rating < 0 OR $rating > 5)
+		{
+			throw new OutOfRangeException('Rating must be in the range of 0.0 to 5.0');
+		}
+		
 		$this->_attributes['rating'] = $rating;
+	}
+	
+	/**
+	 *
+	 * @param type $loc 
+	 * @todo finish.
+	 */
+	public function set_content_segment_loc($loc)
+	{
+		
 	}
 
 	/**
@@ -62,7 +147,7 @@ abstract class Kohana_Sitemap_Video implements Kohana_Sitemap_Interface
 	 */
 	public function set_view_count($view_count)
 	{
-		$this->_attributes['view_count'] = $view_count;
+		$this->_attributes['view_count'] = (int) $view_count;
 	}
 
 	/**
@@ -77,12 +162,22 @@ abstract class Kohana_Sitemap_Video implements Kohana_Sitemap_Interface
 	/**
 	 * @param array $tag Array of tags, a maximum of 32 tags is permitted
 	 */
-	public function set_tag( array $tag )
+	public function set_tag( $tag )
 	{
-		if (count($tag) <= 32)
+		if (is_array($tag))
 		{
-			$this->_attributes['tag'] = $tag;
+			foreach($tag as $row)
+			{
+				$this->set_tag($row);
+			}
 		}
+		
+		if (count($this->_attributes['tags']) > 32)
+		{
+			throw new OverflowException('A maximum of 32 tags are permitted');
+		}
+		
+		$this->_attributes['tags'][] = $tag;
 	}
 
 	/**
@@ -94,6 +189,11 @@ abstract class Kohana_Sitemap_Video implements Kohana_Sitemap_Interface
 	 */
 	public function set_category($category)
 	{
+		if (strlen($category) > 256)
+		{
+			throw new InvalidArgumentException('The category should be no longer than 256 characters long.');
+		}
+		
 		$this->_attributes['category'] = $category;
 	}
 
@@ -103,27 +203,69 @@ abstract class Kohana_Sitemap_Video implements Kohana_Sitemap_Interface
 	 */
 	public function set_family_friendly($family_friendly)
 	{
+		$family_friendly = $family_friendly ? 'yes' : 'no';
+		
 		$this->_attributes['family_friendly'] = $family_friendly;
 	}
 
 	/**
-	 * @param integer $duration 	The duration of the video in seconds. Value must
-	 * be between 0 and 28800 (8 hours). Non-digit characters are disallowed.
+	 * Accepts an array in the form of 
+	 * 
+	 * $array('country' => TRUE|FALSE);
+	 * 
+	 * @param type $countries 
+	 * @todo finish
 	 */
-	public function set_duration($duration)
+	public function set_restriction($countries)
 	{
-		$this->_attributes['duration'] = $duration;
+		
+	}
+	
+	/**
+	 *
+	 * @param type $loc 
+	 * @todo finish
+	 */
+	public function set_gallery_loc($loc)
+	{
+		$this->_attributes['gallery_loc'] = $loc;
+	}
+	
+	/**
+	 * @param float $price The price to download or view the video. The required 
+	 * attribute currency specifies the currency in ISO 4217 format. More than one 
+	 * <video:price> element can be listed (for example, in order to specify various 
+	 * currencies).
+	 * @todo finish
+	 */
+	public function set_price($price)
+	{
+		
+	}
+	
+	/**
+	 * @param bool $subscription Indicates whether a subscription (either paid or free) 
+	 * is required to view the video. Allowed values are yes or no.
+	 */
+	public function set_subscription($subscription)
+	{
+		$subscription = $subscription ? 'yes' : 'no';
+		
+		$this->_attributes['subscription'] = $subscription;
 	}
 
 	/**
-	 * @param integer $expiration_date The date after which the video will no
-	 * longer be available, in unixtimestamp format.
+	 * @param string $uploader A name or handle of the video’s uploader. Only one
+	 * <video:uploader> is allowed per video. The optional attribute info specifies
+	 * the URL of a webpage with additional information about this uploader. This
+	 * URL must be on the same domain as the <loc> tag.
+	 * @todo add info attribute.
 	 */
-	public function set_expiration_date($expiration_date)
+	public function set_uploader($uploader)
 	{
-		$this->_attributes['expiration_date'] = $this->date_format($expiration_date);
+		$this->_attributes['uploader'] = $uploader;
 	}
-
+	
 	public function create()
 	{
 		// Here we need to create a new DOMDocument. This is so we can re-import the
@@ -134,15 +276,29 @@ abstract class Kohana_Sitemap_Video implements Kohana_Sitemap_Interface
 		$video = $document->createElement('video:video');
 
 		$document->appendChild($video);
-
-		// Append attributes
-		foreach($this->_attributes as $name => $value)
+		
+		/**
+		 * Small recursive function to add attributes to the document.
+		 */
+		$append_attributes = function($attributes) use ($video)
 		{
-			if (NULL !== $value)
+			foreach($attributes as $name => $value)
 			{
-				$video->appendChild($document->createElement('video:'.$name, $value));
+				if (NULL !== $value)
+				{
+					if (is_array($value))
+					{
+						$append_attributes($value);
+					}
+					
+					$video->appendChild($document->createElement('video:'.$name, $value));
+				}
 			}
-		}
+		};
+		
+		$append_attributes($this->_attributes);
+		
+		// @todo append: uploader, restriction and player_loc.
 
 		return $video;
 	}
